c623331e69ffb9513bf38e26b42b4d3fc6fa4987
Chase Thomas <chase@chasethomas.com>
2016-10-18 15:53:01 -0400

    plot

--- model_creation.R
+++ model_creation.R
@@ -2,6 +2,9 @@ library(readr)
 library(randomForest)
 library(caret)
 
+library(corrplot)
+library(gridExtra)
+library(ggplot2)
 
 train <- read.csv("train.csv", stringsAsFactors=TRUE)
 test  <- read.csv("test.csv",  stringsAsFactors=TRUE)
@@ -9,7 +12,13 @@ test  <- read.csv("test.csv",  stringsAsFactors=TRUE)
 #Alley, PoolQC, Fence and MiscFeature have WAY more nulls than the other variables (>1000), so remove them
 train<- train[,-c(7,73,74,75)]
 
+nzv <- nearZeroVar(train, saveMetrics= TRUE)
+badCols <- nearZeroVar(train)
+train <- train[, -badCols]
 
+numerical_train <- extractNumeric(train[,!(names(train) %in% col_null)])
+M <- cor(numerical_train)
+corrplot(M, tl.cex = .3)
 
 #partition that data
 partition <- createDataPartition(y=train$SalePrice,
