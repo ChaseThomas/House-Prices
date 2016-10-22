87211ec3f158e3921bbca6154b4ef27173dc7a81
Amit Mizrahi <amit@amizrahi.com>
2016-10-22 14:00:35 -0400

    Replace NA values with median, use only 10 vars for random forest, don't partition

--- model_creation.R
+++ model_creation.R
@@ -36,7 +36,13 @@ extractNumeric <- function(data) {
 numerical_train <- extractNumeric(train)
 
 # delete columns with na values
-nonnan_numerical <- numerical_train[ , colSums(is.na(numerical_train)) == 0]
+numerical_train <- sapply(numerical_train[, colSums(is.na(numerical_train)) > 0], function(col) {
+  col[is.na(col)] <- median(col, na.rm = TRUE)
+});
+str(numerical_train);
+
+#[is.na(x)] <- median(numerical_train$Fare, na.rm = TRUE)
+#nonnan_numerical <- numerical_train[ , colSums(is.na(numerical_train)) == 0]
 
 M <- cor(nonnan_numerical)
 corrplot(M, tl.cex = .3)
@@ -47,17 +53,19 @@ test$MonthAge  = (lubridate::year(Sys.Date()) - test$YrSold)  * 12 + (lubridate:
 str(train)
 
 #partition that data
-partition <- createDataPartition(y=train$SalePrice,
-                                 p=.8,
-                                 list=F)
-training <- train[partition,]
-testing <- train[-partition,]
+#partition <- createDataPartition(y=train$SalePrice,
+#                                 p=.8,
+#                                 list=F)
+#training <- train[partition,]
+#testing <- train[-partition,]
 
 #randomForest Model
-rf <- randomForest(SalePrice ~ ., data=training)
+rf <- randomForest(SalePrice ~ OverallQual + GrLivArea + TotalBsmtSF
+                   + GarageCars + X2ndFlrSF + X1stFlrSF + TotRmsAbvGrd
+                   + BsmtFinSF1 + LotArea + MonthAge, data=train)
 
 # Predict using the test set (code adapted from public Kaggle script in forums and Leo's example)
-prediction <- predict(rf, testing)
+prediction <- predict(rf, test)
 
 solution <- data.frame(HouseID = test$Id, Price = prediction)
-
+write.csv(solution, "house_prices_output.csv", row.names = FALSE)
